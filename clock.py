4f61b36506a72c74daf3ecd94fdc022141e8a841
tirthankar95 <tirthankarmittra@gmail.com>
2024-12-24 09:02:52 +0530

    Add data to excel and fixes
    1. Fix pressing "Pause", "Accumulate", "Save" buttons at the beginning.
    
    2. Add data new day's data to the csv file.

--- clock.py
+++ clock.py
@@ -12,11 +12,10 @@ def modify_datetime(time):
     hrs, mins, secs = [int(x) for x in time.split(':')]
     return hrs * 3600 + mins * 60 + secs
 df['Total seconds'] = df["Time"].apply(lambda x: modify_datetime(x))
-df_total_time = df['Total seconds']
 part1, part2 = st.columns([2, 3])
 part1.dataframe(data = df.style.highlight_max(axis=0), height = 250)
-part2.bar_chart(data = df_total_time, x_label = "Time Spent Studying", y_label = "Frequency",use_container_width = True)
-st.line_chart(data = df_total_time, x_label = "Days", y_label = "Time (in seconds)", use_container_width = True)
+part2.bar_chart(data = df, x = "Day", y = "Total seconds", x_label = "Time Spent Studying", y_label = "Frequency",use_container_width = True)
+st.line_chart(data = df, x = "Day", y = "Total seconds", x_label = "Days", y_label = "Time (in seconds)", use_container_width = True)
 
 t = st.time_input("Set Timer:", value = None)
 try: hv, mv, sv = t.hour, t.minute, 0
@@ -49,7 +48,7 @@ if btn1.button("Start", icon=":material/rocket:", type = "primary"):
         st.session_state["total_time"] -= 1
         time.sleep(1)
 
-if btn2.button("Pause", icon="⏸️"):
+if btn2.button("Pause", icon="⏸️") and "total_time" in st.session_state: 
     with open(os.path.join("data", "save_state"), "w") as files:
         json.dump(dict(st.session_state), files, indent = 5)
     chv, cmv, csv = st.session_state["total_time"] // 3600, \
@@ -65,23 +64,42 @@ st.divider()
 
 ##########################################################################
 btn2, btn3 = st.columns([1, 4]) 
-def log_data():
-    dt = total_time - max(0, st.session_state["total_time"])
-    if "elapsed_time" not in st.session_state: st.session_state["elapsed_time"] = 0
-    st.session_state["elapsed_time"] += dt 
-    del st.session_state["total_time"]
-    with open(os.path.join("data", "save_state"), "w") as files:
-        json.dump(dict(st.session_state), files, indent = 5)
+def get_time():
     hv, mv, sv = str(st.session_state["elapsed_time"] // 3600), \
                  str((st.session_state["elapsed_time"] % 3600) // 60), \
                  str(st.session_state["elapsed_time"] % 60)
     if len(hv) == 1: hv = "0" + hv
     if len(mv) == 1: mv = "0" + mv
     if len(sv) == 1: sv = "0" + sv
-    st.write(f'**Elapsed Time:** {hv}:{mv}:{sv}')
+    return ":".join([hv, mv, sv])
+def log_data():
+    if "total_time" not in st.session_state:
+        st.write(f'**Elapsed Time:** 00:00:00')
+        return
+    dt = max(0, total_time - max(0, st.session_state["total_time"]))
+    if "elapsed_time" not in st.session_state: st.session_state["elapsed_time"] = 0
+    st.session_state["elapsed_time"] += dt 
+    del st.session_state["total_time"]
+    with open(os.path.join("data", "save_state"), "w") as files:
+        json.dump(dict(st.session_state), files, indent = 5)
+    st.write(f'**Elapsed Time:** {get_time()}')
     if st.session_state["elapsed_time"] <= 0: st.balloons()
+
 if btn2.button("Accumulate", icon="🔥", type = "primary"): log_data()
 
 def save_data():
+    global df 
+    if "total_time" not in st.session_state: return
     current_day = df["Day"].max() + 1
+    df = pd.concat([pd.DataFrame({"Day": [current_day], "Time": [get_time()]}), df], ignore_index = True)[["Day", "Time"]]
+    df.to_csv(os.path.join("data", "time_data.csv"), index = False)
+    with open(os.path.join("data", "save_state"), "r") as files:
+        try: temp_dict = json.load(files)
+        except: temp_dict = {}
+        for k, v in temp_dict.items():
+            if isinstance(v, int): temp_dict[k] = 0
+            if isinstance(v, bool): temp_dict[k] = False 
+    with open(os.path.join("data", "save_state"), "w") as files:
+        json.dump(temp_dict, files, indent = 5)
+        
 if btn3.button("Save", icon="💾"): save_data()
